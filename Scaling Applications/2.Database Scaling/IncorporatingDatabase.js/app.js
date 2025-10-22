53ecb92a57cbbde8e2e7fc6b7e1fec5c9f576af2
Ziaulhaq <ziuall>
2025-10-22 21:50:39 +0530

    Incorporated Database

---
+++ Scaling Applications/2.Database Scaling/IncorporatingDatabase.js/app.js
@@ -0,0 +1,18 @@
+const http = require('http')
+const { LocalStorage } = require("node-localstorage")
+
+const db = new LocalStorage("./data")
+// let requests = 0
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        // requests++
+        let requests = db.getItem("requests")
+        db.setItem("requests", ++requests)
+        console.log(`${process.pid}: ${requests}`)
+        res.end(JSON.stringify(requests))
+    }
+})
+
+server.listen(3000)
+console.log(`counting requests`)
